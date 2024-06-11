9fde1912e83320585b6e336068d2fd6563a94a7d
Catalin Pirvu <catapirvu98@gmail.com>
2024-06-11 22:04:55 +0300

    Procesing the User Data

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask.views import MethodView
 from wtforms import Form, StringField, SubmitField
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flatmates_bill import flat
 
 app = Flask(__name__)
 class HomePage(MethodView):
@@ -15,7 +16,15 @@ class BillFormPage(MethodView):
                                billform= bill_form)
 
 class ResultPage(MethodView):
-    pass
+
+    def post(self):
+        billform = BillForm(request.form)
+
+        the_bill = flat.Bill(float(billform.amount.data), billform.period.data)
+        flatmate1 = flat.Flatmate(billform.name1.data, float(billform.days_in_house1.data))
+        flatmate2 = flat.Flatmate(billform.name2.data, float(billform.days_in_house2.data))
+
+        return f"{flatmate1.name} pays {flatmate1.pays(the_bill, flatmate2)}"
 
 class BillForm(Form):
     amount = StringField("Bill Amount: ")
@@ -30,6 +39,9 @@ class BillForm(Form):
     button = SubmitField("Calculate")
 
 app.add_url_rule('/', view_func=HomePage.as_view('home_page'))
-app.add_url_rule('/bill', view_func=BillFormPage.as_view('bill_form_page'))
+app.add_url_rule('/bill',
+                 view_func=BillFormPage.as_view('bill_form_page'))
+app.add_url_rule('/results',
+                 view_func=ResultPage.as_view('result_page'))
 
 app.run(debug=True)
